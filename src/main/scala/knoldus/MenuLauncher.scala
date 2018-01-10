b994e23bc26535aaebec0f2f758c5f90d197119a
jyotisachdeva <jyotisachdeva57@gmail.com>
2018-01-10 17:52:01 +0530

    Initial commit

---
+++ src/main/scala/knoldus/MenuLauncher.scala
@@ -0,0 +1,47 @@
+package knoldus
+
+object MenuLauncher {
+
+  def main(args: Array[String]): Unit = {
+    val obj = new Operation
+    val lvalue1 = 10
+    val lvalue2 = 20
+    val lvalue3 = 70
+    val lvalue4 = 50
+    val list1 = List(lvalue1, lvalue2, lvalue3, lvalue4)
+    val l1value1 = 20
+    val l1value2 = 30
+    val l1value3 = 40
+    val l1value4 = 50
+    val list2 = List(l1value1, l1value2, l1value3, l1value4)
+    val element = 3
+    print("original list1 is ")
+    print(list1)
+    print("\n")
+    print("original list2 is ")
+    print(list1)
+    print("\n")
+    print("reverse list is ")
+    print(obj.reverseList(list1))
+    print("\n")
+    print("After multiplying by 2 list is ")
+    print(obj.listDouble(list1))
+    print("\n")
+    print("length of list is  ")
+    print(obj.listLength(list1))
+    print("\n")
+    print("third element of list1 is ")
+    print(obj.kthElement(element, list1))
+    print("\n")
+    print("area of rectangle and parallelogram using higher order function is ")
+    print(obj.area("rectangle", lvalue1, lvalue2, (a, b) => a * b))
+    print("\n")
+    print("area of rhombus using higher order function ")
+    print(obj.area("rectangle", lvalue1, lvalue2, (a, b) => a * b))
+    print("\nafter adding two lists result is ")
+    print(obj.listsAdd(list1, list2))
+
+  }
+
+
+}
---
+++ src/main/scala/knoldus/Operation.scala
@@ -0,0 +1,77 @@
+package knoldus
+
+class Operation {
+
+  def listDouble(list: List[Int]): List[Int] = {
+    list.map(x => x * 2)
+  }
+
+  def fibonacci(number: Int): Int = {
+    def fibTailRecur(num: Int, first: Int, second: Int): Int = num match {
+      case 0 => first
+      case _ => fibTailRecur(num - 1, second, first + second)
+    }
+
+    fibTailRecur(number, 0, 1)
+  }
+
+  def reverseList(list: List[Int]): List[Int] = {
+    def reverseInternal(res: List[Int], list: List[Int]): List[Int] = {
+      list match {
+        case Nil => res
+        case (head :: tail) => {
+          reverseInternal(head :: res, tail)
+        }
+      }
+    }
+
+    reverseInternal(Nil, list)
+  }
+
+  def listLength(list: List[Int]): Int = {
+
+    def listInternal(list: List[Int]): Int = {
+      list match {
+        case Nil => 0
+        case head :: tail => 1 + listLength(tail)
+      }
+    }
+
+    listInternal(list)
+  }
+
+  def kthElement(element: Int, list: List[Int]): Int = {
+    list match {
+      case Nil => -1
+      case list if (element < (list.length) && element >= 0) => list(element)
+      case _ => -1
+    }
+  }
+
+  def area(shape: String, first: Int, second: Int, f: (Int, Int) => Int): String = {
+    val shapenew = shape.toLowerCase
+    shapenew match {
+      case "rectangle" | "parallelogram" => s"area of $shape is ${f(first, second)}"
+      case "rhombus" => s"area of $shape is  ${f(first, second) / 2}"
+
+    }
+  }
+
+
+  def listsAdd(list1: List[Int], list2: List[Int]): List[Int] = {
+    val lengthl1 = list1.length
+    val lengthl2 = list2.length
+    if (list1.isEmpty) {
+      list2
+    }
+    else if (list2.isEmpty) {
+      list1
+    }
+    else if (lengthl1 == lengthl2) {
+      for ((x, y) <- list1 zip list2) yield (x + y)
+    }
+    else {
+      list1.zipAll(list2, 0, 0).map { case (a, b) => a + b }
+    }
+  }
+}
